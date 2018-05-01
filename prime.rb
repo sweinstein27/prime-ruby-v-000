6b0683d4634ba54111a78d1b8745e94d871a24e8
Scott Weinstein <sweinstein27@gmail.com>
2018-05-01 15:23:20 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 def prime?(number)
   n = number
+return false if n <= 1
 array = [*2..n-1]
-array.each { |number| return false if n <= 1 || n % number == 0 }
+array.each { |number| return false if n % number == 0 }
   return true
 end
