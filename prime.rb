6038e9046e5cbd7533f1cde630d73c26e8dedf2d
Scott Weinstein <sweinstein27@gmail.com>
2018-05-01 15:00:51 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,10 @@
 # end
 
 def prime?(n)
-  return false if n < 2 or n % 2 == 0
-  return true if n == 2
-  for d in 3..Math.sqrt(n).to_i
-    return false if n % d == 0
-  end
-  return true;
-end
+      for d in 2..(n - 1)
+       if (n % d) == 0
+        return false
+       end
+      end
+     true
+   end
