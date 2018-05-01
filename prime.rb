4780265916a0b2e17cd2964c628d1da05830b659
Scott Weinstein <sweinstein27@gmail.com>
2018-05-01 15:02:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,11 @@
-# def prime?(number)
-#   n = number.abs
-#   array = [*2..n-1]
-# array.each { |number| return false if n <= 1 || n % number == 0 }
-#   return true
-# end
+def prime?(number)
+  n = number.abs
+if n < 0
+  false
+else
+  array = [*2..n-1]
+array.each { |number| return false if n <= 1 || n % number == 0 }
+  return true
+end
+end
 
-def prime?(n)
-      for d in 2..(n - 1)
-       if (n % d) == 0
-        return false
-       end
-      end
-     true
-   end
